f4511feb3f38d9647e225b60b9fe76252918d765
lashaloladze <llola16@freeuni.edu.ge>
2019-09-12 23:24:54 +0400

    updated about us

--- views/about.php
+++ views/about.php
@@ -24,7 +24,7 @@
                   <li><a href="#" onclick="$('#about_cont').empty().load(suggestions);"><?=$translator->translate("რჩევები მყიდველს")?></a></li>
                 </ul>
               </div>
-              <div class="col-md-8 about-content" id="about_cont">
+              <div class="col-md-8 about-content about-content-left" id="about_cont" style="-webkit-backdrop-filter: blur(30px);backdrop-filter: blur(30px);background-color: #ffffff;">
               </div>
             </div>
             <!-- /.row (nested) -->
