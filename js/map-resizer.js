faf2d850b9049f3be7cc9af3a9ce1b764f6ac637
Gogotchuri <igogo17@freeuni.edu.ge>
2019-09-13 04:45:11 +0400

    a

--- js/map-resizer.js
+++ js/map-resizer.js
@@ -5,4 +5,18 @@ $(document).ready(function(e) {
         fillColor: 'ffb600',
         fillOpacity: 0.5
     });
+
+    $("#1st").click(function() {
+        alert('clicked')
+    })
+});
+jQuery(window).bind('resize orientationchange', function(e) {
+    var windowWidth = $(window).width();
+    jQuery(window).resize(function() {
+        jQuery('img[usemap]').maphilight({
+            stroke: false,
+            fillColor: 'ffb600',
+            fillOpacity: 0.5
+        });
+    })
 });
