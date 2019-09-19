7f088714370dc88832635266cacd164a23ac7dad
lashaloladze <llola16@freeuni.edu.ge>
2019-09-19 16:56:06 +0400

    deleted timer

--- views/home.php
+++ views/home.php
@@ -28,16 +28,6 @@
 <section class="new-landing">
     <a href="/project?ID=9" class="new-landing-main" style="background: url(<?=$project_thumbnail?>)">
         <img src="<?=$project_thumbnail?>" style="opacity: 0">
-        <div id="timer">
-            <span class="left"><?=$translator->translate("დარჩენილია")?></span>
-            <div id="days"></div>
-            <span class="two-dots">:</span>
-            <div id="hours"></div>
-            <span class="two-dots">:</span>
-            <div id="minutes"></div>
-            <span class="two-dots">:</span>
-            <div id="seconds"></div>
-        </div>
     </a>
     <div class="new-landing-right">
         <div class="new-landing-header">
@@ -74,33 +64,6 @@
 <!-- /Landing-->
 
 <script>
-    // Countdown timer
-    var countDownDate = new Date("Dec 30, 2019 23:59:59").getTime();
-    var x = setInterval(function () {
-        // Get today's date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Display the result in the element with id="demo"
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
-
-        // If the count down is finished, write some text
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("timer").innerHTML = "EXPIRED";
-        }
-    },1000);
 
     //
     var slideIndex = 1;
